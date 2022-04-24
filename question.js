60aa8e4175e3adb741135eddef8b7dcb04e77d34
KALAIVANAN95 <kalaivanasrm1995@gmail.com>
2022-04-24 19:37:37 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,56 @@
+const quiz=[{
+    q:'Which month comes right before june?',
+    options:['may','september','july','august'],
+    answer:0
+},
+{
+    q:'What color is a banana?',
+    options:['red','yellow','white','blue'],
+    answer:1
+},
+{
+    q:'3 + 4 = 7 ?',
+    options:['true','false'],
+    answer:0
+},
+{
+    q:'What time of day do we have breakfast?',
+    options:['In the afternoon','In the evening','In the morning'],
+    answer:2
+},
+
+{
+    q:'What is 22 + 6?',
+    options:['99','56','16','28'],
+    answer:3
+},
+
+{
+    q:'8 - 7 = 1 ?',
+    options:['true','false'],
+    answer:0
+},
+
+{
+    q:'What color is a apple?',
+    options:['red','yellow','white','blue'],
+    answer:0
+},
+{
+    q:'What is 100 + 2?',
+    options:['100','102','99','78'],
+    answer:1
+},
+{
+    q:'Which month comes right before september?',
+    options:['may','september','july','august'],
+    answer:3
+
+},
+{
+    q:'What time of day do we have dinner?',
+    options:['In the afternoon','In the night','In the morning'],
+    answer:1
+}
+
+]
